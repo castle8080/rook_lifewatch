d51639d8e2993609ef3f72c57af564c0980a19a7
bryanc <castillo.bryan@gmail.com>
2026-01-16 13:56:55 -0800

    Checking if www exists or not.

--- rook_lw_admin/scripts/install.sh
+++ rook_lw_admin/scripts/install.sh
@@ -4,5 +4,7 @@ set -e
 mkdir -p ../dist/bin
 cp -vf target/release/rook_lw_admin ../dist/bin/rook_lw_admin
 
-mkdir -p ../dist/www
-cp -rvf www/ ../dist/
+if [ -d www ]; then
+    mkdir -p ../dist/www
+    cp -rvf www/ ../dist/
+fi
