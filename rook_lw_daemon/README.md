cb0bb0da7f874a6253f25def9fa1d4ca3e74d777
Bryan Castillo <castillo.bryan@gmail.com>
2026-01-22 13:40:55 -0800

    Getting the daemon to compile on windows.

--- rook_lw_daemon/README.md
+++ rook_lw_daemon/README.md
@@ -6,6 +6,9 @@ Runs video motion capture.
 
 Dependin on the features in the build some extra dependencies may be required.
 
+### Clang
+You will need libclang.
+
 ### opencv
 
 To use opencv, dependcies need to be installed to build.
@@ -21,3 +24,7 @@ The app needs sqlite3.
 ```
 sudo apt install sqlite3 libsqlite3-dev
 ```
+
+## Windows Support
+
+There is some support for building and running on windows, but not well tested. It won't use libcamera, but opencv may work. You need to install clang and have it on the path and also need to download binaries for openc unpacked to c:\opencv.
--- rook_lw_daemon/make.py
+++ rook_lw_daemon/make.py
@@ -3,19 +3,70 @@ import sys
 import subprocess
 import shutil
 import os
+import platform
 
 install_dir = "../dist"
 bin_dir = os.path.join(install_dir, "bin")
 build_dir = "target/release"
 
+windows_opencv_dir = os.environ.get('OpenCV_DIR', 'C:\\opencv\\build')
+
+def _setup_opencv():
+    if platform.system() == 'Windows':
+        # Download binaries from: https://opencv.org/releases/
+        # and place them under C:\\opencv
+        
+        # Need to setup environment variables telling well opencv is.
+        if not os.path.isdir(windows_opencv_dir):
+            raise Exception(f"No opencv build directory found.")
+        
+        # Get include dir
+        opencv_inc_dir = os.path.join(windows_opencv_dir, 'include')
+        
+        opencv_lib_dir = None
+        opencv_bin_dir = None
+        print(windows_opencv_dir)
+        for f_ent in os.listdir(os.path.join(windows_opencv_dir, 'x64')):
+            if f_ent.lower().startswith("vc"):
+                print(f_ent)
+                _bin_dir = os.path.join(windows_opencv_dir, 'x64', f_ent, 'bin')
+                _lib_dir = os.path.join(windows_opencv_dir, 'x64', f_ent, 'lib')
+                if os.path.isdir(_bin_dir) and opencv_bin_dir is None:
+                    opencv_bin_dir = _bin_dir
+                if os.path.isdir(_lib_dir) and opencv_lib_dir is None:
+                    opencv_lib_dir = _lib_dir
+        if opencv_lib_dir is None or opencv_bin_dir is None:
+            raise Exception(f"No opencv lib/bin directory found.")
+        
+        os.environ['PATH'] = opencv_bin_dir + ';' + os.environ['PATH']
+
+        os.environ['OPENCV_LINK_LIBS'] = 'opencv_world4120'
+        os.environ['OPENCV_INCLUDE_PATHS'] = opencv_inc_dir
+        os.environ['OPENCV_LINK_PATHS'] = opencv_lib_dir
+
+        # Needed by rust crate on windows to skip trying pkg-config
+        os.environ['OPENCV_DISABLE_PROBES'] = 'pkg_config,cmake,vcpkg_cmake,vcpkg'
+    else:
+        # Nothing to do expect that pkg-config finds it.
+        pass
+
+def _get_features():
+    if platform.system() == 'Windows':
+        # Libcamera is linux.
+        return []
+    else:
+        return ["--features", "libcamera"]
+    
 def clean():
     subprocess.run(["cargo", "clean"], check=True)
 
 def test():
-    subprocess.run(["cargo", "test", "--release", "--features", "libcamera"], check=True)
+    _setup_opencv()
+    subprocess.run(["cargo", "test", "--release"] + _get_features(), check=True)
 
 def build():
-    subprocess.run(["cargo", "build", "--release", "--features", "libcamera"], check=True)
+    _setup_opencv()
+    subprocess.run(["cargo", "build", "--release"] + _get_features(), check=True)
 
 def _install_executables():
     os.makedirs(bin_dir, exist_ok=True)
